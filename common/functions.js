a99fd46b697db53ba419109277963a04fea68dad
ParkJeongbeen <pjb6510@gmail.com>
2021-08-07 18:28:37 +0900

    Add reduce

--- common/functions.js
+++ common/functions.js
@@ -9,3 +9,29 @@ _.map = (callback, iter) => {
 
   return result;
 };
+
+_.filter = (callback, iter) => {
+  const result = [];
+
+  for (const value of iter) {
+    if (callback(value)) {
+      result.push(value);
+    }
+  }
+
+  return result;
+};
+
+_.reduce = (callback, acc, iter) => {
+  if (iter === undefined) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+
+  for (const value of iter) {
+    acc = callback(acc, value);
+  }
+
+  return acc;
+};
+
